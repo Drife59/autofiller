5ce733934887af099a9abaceeb717cc212ce31d9
Benjamin <benjamingrassart@togetheer.com>
2018-02-26 16:52:16 +0100

    Admin controller
    Should work

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -25,42 +25,57 @@ namespace Application_WEB_MVC.Controllers
 
         //Replace a pivot in db TODO
         [HttpPost]
-        [Route("/admin/pivot/apply_merge/{pivot_origine}/{pivot_remplacement}")]
-        public IActionResult remplacement_direct(int pivot_origine_id, int pivot_remplacement_id){
+        [Route("/admin/merges/apply/{merge_id}/{pivot_to_keep_id}")]
+        public IActionResult remplacement_direct(int merge_id, int pivot_to_keep_id){
 
-            var pivot_orig = _context.Pivots
-                .Where(p => p.pivotId == pivot_origine_id)
+            var merge = _context.Merges
+                .Where(m => m.mergeId == merge_id)
                 .FirstOrDefault();
 
-            var pivot_remp = _context.Pivots
-                .Where(p => p.pivotId == pivot_remplacement_id)
+            if( merge == null){
+                return NotFound("Merge does not exist");
+            }
+
+            var pivot_to_keep = _context.Pivots
+                .Where(p => p.pivotId == pivot_to_keep_id)
                 .FirstOrDefault();
 
+            if( pivot_to_keep == null){
+                return BadRequest("Pivot to keep does not exist");
+            }
+
+            var pivot_to_replace = merge.PivotFromUser;
+            if( merge.PivotFromUser == pivot_to_keep ){
+                pivot_to_replace = merge.PivotFromWebsite;
+            }
+
+            //Get user value to be modified
             var user_value = _context.UserValues
-                .Where(uv => uv.Pivot == pivot_orig)
+                .Where(uv => uv.Pivot == pivot_to_replace)
                 .ToList();
 
             foreach(UserValue uv in user_value){
-                uv.Pivot = pivot_remp;
+                uv.Pivot = pivot_to_keep;
                 _context.Add(uv);
             }
 
+            //Get keys to be modified
             var keys = _context.Keys
-                .Where(k => k.Pivot == pivot_orig)
+                .Where(k => k.Pivot == pivot_to_replace)
                 .ToList();
 
             foreach(Key k in keys){
-                k.Pivot = pivot_remp;
+                k.Pivot = pivot_to_keep;
                 _context.Add(k);
             }
             _context.SaveChanges();
             
-            return Ok("remplacement de " + pivot_orig.name + " par " + pivot_remp.name);
+            return Ok("remplacement de " + pivot_to_replace.name + " par " + pivot_to_keep.name);
         }
 
         //Record a new merge request
         [HttpPost]
-        [Route("/admin/pivot/remplacement_async/{pivot_origine}/{pivot_remplacement}")]
+        [Route("/admin/merge/{pivot_origine}/{pivot_remplacement}")]
         public IActionResult remplacement_async(string pivot_origine, string pivot_remplacement,
                                                 [FromBody] MergeRequest merge_request){
             
@@ -78,23 +93,39 @@ namespace Application_WEB_MVC.Controllers
                 .Where(u => u.email == merge_request.email) 
                 .FirstOrDefault();
 
+            if( user == null){
+                return BadRequest("User cannot be found");
+            }
+
             merge.User = user;
 
             var website = _context.Websites
                 .Where(w => w.domaine == merge_request.domaine)
                 .FirstOrDefault();
 
+            if( website == null){
+                return BadRequest("Website cannot be found");
+            }
+
             merge.Website = website;
 
             var pivot_orig = _context.Pivots
                 .Where(p => p.name == pivot_origine)
                 .FirstOrDefault();
+            
+            if( pivot_orig == null){
+                return BadRequest("Origin pivot cannot be found");
+            }
 
             merge.PivotFromUser = pivot_orig;
 
             var pivot_remp = _context.Pivots
                 .Where(p => p.name == pivot_remplacement)
                 .FirstOrDefault();
+
+            if( pivot_remp == null){
+                return BadRequest("Remplacement pivot cannot be found");
+            }
             
             merge.PivotFromWebsite = pivot_remp;
 
